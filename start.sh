011e974fe9925205ae4342364b1f09f237888776
AKSizov <37084670+AKSizov@users.noreply.github.com>
2021-02-16 11:51:19 -0500

    removed hpet for stuttering

--- start.sh
+++ start.sh
@@ -101,6 +101,7 @@ sudo $z_SHIELD_COMMAND "time sudo qemu-system-x86_64 \
 	-rtc base=localtime,clock=host,driftfix=none `# windows needs localtime rtc`\
 	-smp ${CPUS},sockets=1,cores=${z_CORES},threads=2 `# CPU topology`\
 	--enable-kvm `# so we can actually get some speed`\
+	-no-hpet `# no high precision event timer to fix stutters`\
 	-mem-prealloc `# prealloc memory`\
 	-global ICH9-LPC.disable_s3=1 `# no idea`\
 	-global ICH9-LPC.disable_s4=1 `# no idea`\
