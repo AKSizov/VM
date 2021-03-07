a56263e2e569e28f62bf45b829c3596c169d287d
AKSizov <37084670+AKSizov@users.noreply.github.com>
2021-03-07 13:13:41 -0500

    changed realtime settings

--- start.sh
+++ start.sh
@@ -143,11 +143,14 @@ sudo bash -c "echo 303 > /sys/bus/workqueue/devices/writeback/cpumask" # cpu bit
 echo "==> shutting down picom..."
 pkill picom # <= this little shit was the cause of all of my problems.
 # keep this here if you experience lag in the guest when doing literally anything in the host
-echo "==> setting cpu freq"
+echo "==> setting cpu freq..."
 sudo ./freq-max.sh
+echo "==> changing rt settings..."
+echo -1 | sudo tee /proc/sys/kernel/sched_rt_runtime_us # don't limit cpu to 95% (realtime tasks are throttled to 95% to prevent system lock-ups)
+# https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_for_real_time/7/html/tuning_guide/real_time_throttling
 echo "==> start the monstrosity..."
 # sudo $z_SHIELD_COMMAND
-sudo bash -c "time sudo qemu-system-x86_64 \
+sudo bash -c "time sudo chrt -f 99 qemu-system-x86_64 \
 	-name win10,debug-threads=on `# if we need to take the treads from somewhere else`\
 	-pidfile /run/qemu_ex.pid \
 	-pflash OVMF-Custom.fd `# UEFI image`\
