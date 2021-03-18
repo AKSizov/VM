a5a9d01488e59e94be8b9be628bcfa2fc6909aae
AKSizov <37084670+AKSizov@users.noreply.github.com>
2021-03-18 10:54:30 -0400

    removed flags that didn't help

--- start.sh
+++ start.sh
@@ -105,9 +105,6 @@ sudo bash -c "echo 303 > /sys/bus/workqueue/devices/writeback/cpumask" # cpu bit
 echo "==> changing rt settings..."
 echo -1 | sudo tee /proc/sys/kernel/sched_rt_runtime_us # don't limit cpu to 95% (realtime tasks are throttled to 95% to prevent system lock-ups)
 # https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux_for_real_time/7/html/tuning_guide/real_time_throttling
-echo 2 | sudo tee /proc/sys/kernel/sched_nr_migrate
-echo RT_RUNTIME_GREED | sudo tee /sys/kernel/debug/sched_features
-echo NO_RT_RUNTIME_SHARE | sudo tee /sys/kernel/debug/sched_features
 echo "==> start the monstrosity..."
 # sudo $z_SHIELD_COMMAND
 sudo bash -c "time sudo qemu-system-x86_64 \
