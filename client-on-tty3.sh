a551b26ab6168a5cd94665962b9abdc0704d4534
AKSizov <37084670+AKSizov@users.noreply.github.com>
2021-03-24 15:27:31 -0400

    for lg client

--- client-on-tty3.sh
+++ client-on-tty3.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-xinit /usr/bin/firefox $* -- :1 vt3
+cd /tools/vm/looking-glass/client/build
+SDL_VIDEO_X11_VISUALID=0x022 xinit /tools/vm/looking-glass/client/build/looking-glass-client -k -m 73 -a -s -S $* -- :1 vt3
