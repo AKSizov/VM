ebf282dfc58a63932d3298e1365298efeb8657ed
AKSizov <37084670+AKSizov@users.noreply.github.com>
2021-03-30 10:48:44 -0400

    looking glass client on seperate tty

--- client-on-tty3.sh
+++ client-on-tty3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /tools/vm/looking-glass/client/build
-SDL_VIDEO_X11_VISUALID=0x022 xinit /tools/vm/looking-glass/client/build/looking-glass-client -k -m 73 -a -s -S $* -- :1 vt3
+SDL_VIDEO_X11_VISUALID=0x022 xinit /usr/bin/bash -c "xrandr --output eDP-1 --mode 1920x1080 && /tools/vm/looking-glass/client/build/looking-glass-client -m 73 -a -s -T -S win:quickSplash=yes win:position=0x0 egl:vsync=yes egl:multisample=no opengl:vsync=yes" $* -- :1 vt3 -config xorg.conf.d/z_alt.altconfig
