77ffbf84d42ff062d629d16b2f4e84c3056b9f06
AKSizov <37084670+AKSizov@users.noreply.github.com>
2021-05-04 09:10:01 -0400

    added new looking glass start script

---
+++ client.sh
@@ -0,0 +1,8 @@
+#cd /tools/vm/looking-glass/client/build
+#SDL_VIDEO_X11_VISUALID=0x022 ./looking-glass-client \
+#	-k \
+#	-m 73 \
+#	-a \
+#	-S \
+#	-s
+/tools/vm/sources/looking-glass-B3-174-gc6d7fb8dd0/client/build/looking-glass-client -S -s -F
