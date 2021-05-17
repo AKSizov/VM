0e5780d45a98a8966e82d4e6f1e1deec8143da08
AKSizov <37084670+AKSizov@users.noreply.github.com>
2021-05-17 09:20:06 -0400

    updated lg client and host

--- client.sh
+++ client.sh
@@ -5,4 +5,4 @@
 #	-a \
 #	-S \
 #	-s
-/tools/vm/sources/looking-glass-B3-174-gc6d7fb8dd0/client/build/looking-glass-client -S -s -F
+/tools/vm/sources/looking-glass-B3-180-g1a530da139/client/build/looking-glass-client -S -s -F
